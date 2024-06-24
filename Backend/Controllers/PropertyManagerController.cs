4e842d52f7580539fae4427de795da2d34e64997
DavidBudnitskyBBD <davidbu@bbd.co.za>
2024-06-24 18:18:04 +0200

    Add the controller and service we will use

---
+++ Backend/Controllers/PropertyManagerController.cs
@@ -0,0 +1,49 @@
+﻿using Backend.DTOs;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+	/// <summary>
+	/// Property manager controller
+	/// </summary>
+	[ApiController]
+	[Route("PropertyManager")]
+	public class ProperyManagerController : ControllerBase
+	{
+
+		private readonly ILogger<ProperyManagerController> _logger;
+		private readonly IPropertyManagerService _propertyManagerService;
+
+		public ProperyManagerController(ILogger<ProperyManagerController> logger, IPropertyManagerService propertyManagerService)
+		{
+			_logger = logger;
+			_propertyManagerService = propertyManagerService;
+		}
+
+		/// <summary>
+		/// Summary
+		/// </summary>
+		/// <returns>---------</returns>
+		/// <remarks>
+		/// 
+		/// Remarks go here
+		///
+		/// </remarks>
+		/// <response code="200"> Good </response>
+		/// <response code="400"> Bad </response>
+		[HttpPut(Name = "spawn")]
+		public IActionResult SpawnProperties()
+		{
+			try
+			{
+				_propertyManagerService.SpawnProperties();
+				return Ok();
+			}
+			catch(Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+	}
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -12,7 +12,7 @@ namespace Backend
 
 			// Add services to the container.
 			builder.Services.AddControllers();
-			builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+			builder.Services.AddScoped<IPropertyManagerService, PropertyManagerService>();
 
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();

0f0144a10a1a1d53aedba2503be697b339fb0447
Lior-Becker <lior.becker@bbd.co.za>
2024-06-26 08:06:17 +0200

    fixed end point names and swagger documentation

--- Backend/Controllers/PropertyManagerController.cs
+++ Backend/Controllers/PropertyManagerController.cs
@@ -21,45 +21,35 @@ namespace Backend.Controllers
 			_propertyManagerService = propertyManagerService;
 		}
 
+        /// <summary>
+        /// Used to start the simulation, and creaste 5000 random properties
+        /// </summary>
+        /// <returns>Confirmation on amount created</returns>
+        /// <remarks>
+        /// 
+        /// This is meant for the initial propery spawn
+		/// No body is required
+		/// Requires an authorization header
+        ///
+        /// </remarks>
+        /// <response code="200"> Good </response>
+        /// <response code="400"> Bad </response>
+        [HttpPost("initialize", Name = "initialize")]
+        public IActionResult SpawnProperties()
+        {
+            try
+            {
+                int numCreated = _propertyManagerService.SpawnProperties(5000);
+                return Ok(numCreated + " properties were created");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-		[HttpGet("test")]
-		public IActionResult GetProps()
-		{
-			try
-			{
-				var answer= _propertyManagerService.GetTop5Properties();
-				return Ok(answer);
-			}
-			catch(Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
-		}
 
-		/// <summary>
-		/// Used to create 50 new properties
-		/// </summary>
-		/// <returns>Confirmation on amount created</returns>
-		/// <remarks>
-		/// 
-		/// This is meant for the initial propery spawn
-		///
-		/// </remarks>
-		/// <response code="200"> Good </response>
-		/// <response code="400"> Bad </response>
-		[HttpPost("spawn", Name = "spawn")]
-		public IActionResult SpawnProperties()
-		{
-			try
-			{
-				int numCreated = _propertyManagerService.SpawnProperties(50);
-				return Ok(numCreated + " properties were created");
-			}
-			catch(Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
-		}
+
 
 		/// <summary>
 		/// Used to set the new price of the property
@@ -88,30 +78,6 @@ namespace Backend.Controllers
 		}
 
 
-		/// <summary>
-		/// Used to create a variable amount of new properties
-		/// </summary>
-		/// <returns>Confirmation on amount created</returns>
-		/// <remarks>
-		/// 
-		/// This is meant to add new properties later on in the world
-		///
-		/// </remarks>
-		/// <response code="200"> Good </response>
-		/// <response code="400"> Bad </response>
-		[HttpPost("spawn/{num:int}", Name = "spawn_more")]
-		public IActionResult SpawnMoreProperties(int num)
-		{
-			try
-			{
-				int numCreated = _propertyManagerService.SpawnProperties(num);
-				return Ok(numCreated + " properties were created");
-			}
-			catch(Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
-		}
 
         /// <summary>
         /// Used to get an available property of a given size
@@ -119,12 +85,19 @@ namespace Backend.Controllers
         /// <returns>Property ID and price of the requested property</returns>
         /// <remarks>
         /// 
-        /// requires the query param size
+        /// the body is strucutred as follows:
+		/// {
+		/// size:int
+		/// renting:boolean
+		/// sellerId:long -> also acts as the land lord ID
+		/// buyerId:long -> this also acts as the renter ID
+		/// 
+		/// }
         ///
         /// </remarks>
         /// <response code="200"> Good: body is json that includes the property ID and the price of the property </response>
         /// <response code="400"> Bad: used if missing a query paramter or when there is an error with the request</response>
-        [HttpPost(Name = "Property")]
+        [HttpPost("property",Name = "Request_poperty")]
 		public IActionResult GetProperties()
 		{
 			try
@@ -145,5 +118,60 @@ namespace Backend.Controllers
 				return BadRequest(ex.Message);
 			}
 		}
-	}
+
+        /// <summary>
+        /// Used to get the owner ID of a given property
+        /// </summary>
+        /// <returns>owner ID of the requested property</returns>
+        /// <remarks>
+        /// 
+        /// requires the poperty ID in the url
+        ///
+        /// </remarks>
+        /// <response code="200"> Good: body is json that includes the property ID and the price of the property </response>
+        /// <response code="400"> Bad: used if missing a query paramter or when there is an error with the request</response>
+        [HttpGet("Owner/{propertyID}", Name ="Get_Owner")]
+		public IActionResult GetOwner()
+		{
+			return(Ok());	
+		}
+
+        /// <summary>
+        /// Used to list a property on the market to be sold
+        /// </summary>
+		/// 
+        /// <returns>200 or a 400</returns>
+        /// <remarks>
+        /// 
+        /// Body requres ownerID
+        ///
+        /// </remarks>
+        /// <response code="200"> Good: property is now listed on the market </response>
+        /// <response code="400"> Bad:failed to list property</response>
+        [HttpPost("sell", Name = "sell_property")]
+        public IActionResult sellProperty()
+        {
+            return (Ok());
+        }
+
+
+
+        /// <summary>
+        /// Used to list a property on the market to be rented
+        /// </summary>
+        /// 
+        /// <returns>200 or a 400</returns>
+        /// <remarks>
+        /// 
+        /// Body requres ownerID
+        ///
+        /// </remarks>
+        /// <response code="200"> Good: property is now rentable </response>
+        /// <response code="400"> Bad:failed to list property</response>
+        [HttpPost("rent", Name = "rent_property")]
+        public IActionResult rentProperty()
+        {
+            return (Ok());
+        }
+    }
 }
